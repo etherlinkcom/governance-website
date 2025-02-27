efe31d1719d277df2d0ccd2f52efc45f955b34ec
Tim McMackin <timothymcmackin@yahoo.com>
2025-02-27 15:56:56 -0500

    Move proposal data to separate file

--- src/app/components/voting/common/informationLink/informationLink.tsx
+++ src/app/components/voting/common/informationLink/informationLink.tsx
@@ -1,24 +1,11 @@
 import styles from "./informationLink.module.css";
 
+// Data about proposals and links to information about them
+import { allLinkData } from "@/data/proposals";
+
 interface InformationLinkProps {
   cycle: number,
 }
-
-// Data about proposals and links to information about them
-const allLinkData = [
-  {
-    periodType: 'kernel',
-    cycles: [134, 135],
-    href: 'https://medium.com/@etherlink/announcing-calypso-the-next-etherlink-upgrade-proposal-dbe92c576da9',
-    title: 'Announcing Calypso: The Next Etherlink Upgrade Proposal',
-  },
-  {
-    periodType: 'kernel',
-    cycles: [83],
-    href: 'https://medium.com/etherlink/announcing-bifr%C3%B6st-a-2nd-upgrade-proposal-for-etherlink-mainnet-ef1a7cf9715f',
-    title: 'Announcing Bifröst: a 2nd upgrade proposal for Etherlink Mainnet',
-  },
-];
 
 export const InformationLink = ({ cycle }: InformationLinkProps) => {
 
---
+++ src/data/proposals.ts
@@ -0,0 +1,16 @@
+// Info with links to web pages about proposals
+
+export const allLinkData = [
+  {
+    periodType: 'kernel',
+    cycles: [134, 135],
+    href: 'https://medium.com/@etherlink/announcing-calypso-the-next-etherlink-upgrade-proposal-dbe92c576da9',
+    title: 'Announcing Calypso: The Next Etherlink Upgrade Proposal',
+  },
+  {
+    periodType: 'kernel',
+    cycles: [83],
+    href: 'https://medium.com/etherlink/announcing-bifr%C3%B6st-a-2nd-upgrade-proposal-for-etherlink-mainnet-ef1a7cf9715f',
+    title: 'Announcing Bifröst: a 2nd upgrade proposal for Etherlink Mainnet',
+  },
+];
