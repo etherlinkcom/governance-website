2004b5912c0a52b7de31fb4d51d5b14463655ecd
Devon <devnathan94@gmail.com>
2025-07-08 16:31:27 +0400

    chore: info icon to progress bar

--- src/app/components/common/progress.tsx
+++ src/app/components/common/progress.tsx
@@ -4,6 +4,7 @@ import { formatPercentageCompact } from '@/lib/governance/utils';
 import { Progress as ProgressAnt } from 'antd';
 import clsx from 'clsx';
 import { appTheme } from '.';
+import { InfoIcon } from './infoIcon';
 
 interface ProgressProps {
   value: BigNumber;
@@ -16,22 +17,29 @@ export const Progress = ({ value, target, text, className }: ProgressProps) => {
   const progressValue = value.dividedBy(target).toNumber() * 100;
   const title = `Current voting power: ${formatPercentageCompact(value)}; Required: ${formatPercentageCompact(target)}`;
 
-  return <div className={clsx('flex flex-col', className)} title={title}>
-    <div className="flex flex-row gap-6 justify-between">
-      <span>{text}:</span>
-      <span className={value.gte(target) ? appTheme.accentTextColor : appTheme.redTextColor}>
-        {`${formatPercentageCompact(value)} / ${formatPercentageCompact(target)}`}
-      </span>
+  return (
+    <div className={clsx('flex flex-col', className)} title={title}>
+      <div className="flex flex-row items-center gap-2 justify-between">
+        <span>{text}:</span>
+        <span className={clsx(
+          value.gte(target) ? appTheme.accentTextColor : appTheme.redTextColor,
+          'flex items-center gap-1'
+        )}>
+          {`${formatPercentageCompact(value)} / ${formatPercentageCompact(target)}`}
+          <InfoIcon className={`w-4 h-4 ${appTheme.accentColorValue}`} />
+        </span>
+      </div>
+      <ProgressAnt
+        style={{ fontSize: 4 }}
+        size={['small', 2]}
+        percent={progressValue}
+        showInfo={false}
+        strokeColor={value.gte(target) ? appTheme.accentColorValue : appTheme.redColorValue}
+        trailColor={appTheme.borderColorValue}
+        status={value.gte(target) ? 'success' : 'exception'}
+      />
     </div>
-    <ProgressAnt
-      style={{ fontSize: 4 }}
-      size={['small', 2]}
-      percent={progressValue}
-      showInfo={false}
-      strokeColor={value.gte(target) ? appTheme.accentColorValue : appTheme.redColorValue}
-      trailColor={appTheme.borderColorValue}
-      status={value.gte(target) ? 'success' : 'exception'} />
-  </div>
+  );
 };
 
 export const ProgressPure = memo(Progress);
